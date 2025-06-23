c01d3dd59b8408c8e9b82d5e44d6ac5a58ee0217
Lê Duy Hải <leduyhai090704@gmail.com>
2025-06-24 01:00:59 +0700

    Initial commit with .py and .txt files

---
+++ send_temperature_vn.py
@@ -0,0 +1,38 @@
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
+import requests
+import time
+
+# ====== Thông tin kết nối InfluxDB ======
+token = "mytoken123"
+org = "my-org"
+bucket = "temperature"
+url_influx = "http://localhost:8086"
+
+client = InfluxDBClient(url=url_influx, token=token, org=org)
+write_api = client.write_api(write_options=SYNCHRONOUS)
+
+# ====== Thông tin kết nối API thời tiết ======
+API_KEY = "04ac04c6c5dfbc0ed71a2f734493c394"
+CITY = "Ho Chi Minh"
+URL = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+# ====== Gửi dữ liệu nhiệt độ thực tế mỗi 5 giây ======
+while True:
+    try:
+        res = requests.get(URL)
+        data = res.json()
+        #print(data)  # ✅ In toàn bộ dữ liệu để bạn dễ debug
+
+        if "main" in data:
+            nhiet_do = data["main"]["temp"]
+            point = Point("temperature_data").field("value", nhiet_do)
+            write_api.write(bucket=bucket, org=org, record=point)
+            print(f"✅ Gửi nhiệt độ thực tế: {nhiet_do}°C")
+        else:
+            print("❌ Lỗi khi gọi API: 'main'")
+            print("Không lấy được dữ liệu. Bỏ qua lần này.")
+    except Exception as e:
+        print(f"❌ Lỗi: {e}")
+    
+    time.sleep(5)
